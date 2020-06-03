7c80f617a0b1c3cc0cd5fb06eb355aa36638ac4f
jany <adnanxheikh1973@gmail.com>
2020-06-03 14:28:07 +0500

    lines

--- variables.R
+++ variables.R
@@ -1,13 +1,1 @@
-# vectors
-
-# logical
-
-vtr1=c(TRUE,FALSE)
-vtr2=c(15,34.786747,999)
-vtr3=c(20L,50L,89L)
-vtr4=c(23L)
-vtr5=c(TRUE,34.89,67L)
-vtr6=c(FALSE,"go",90)
-class(vtr1)
-class(vtr2)
-class(vtr3)
+mtr = matrix(c(5:29),5,5)
